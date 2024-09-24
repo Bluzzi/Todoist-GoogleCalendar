1233fd81423685e9d4738b5bc8696b0d57f57c3e
hcampos <hcampospro@gmail.com>
2024-09-24 19:14:12 +0200

    feat(database): add migration and schema for linking events with Todoist projects

---
+++ prisma/migrations/20240924171232_add_link_between_events_and_todoist_projects/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "EventsSyncProjects" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "projectID" TEXT NOT NULL,
+
+    CONSTRAINT "EventsSyncProjects_pkey" PRIMARY KEY ("id")
+);
